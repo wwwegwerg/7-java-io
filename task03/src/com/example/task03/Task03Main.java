6ac2cfda7162ea55d7d77084b4ccbf23ed406a40
Daniil <xmegatech89@gmail.com>
2026-02-26 18:19:16 +0500

    tasks 1-4

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,21 +1,17 @@
 package com.example.task03;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
 
 public class Task03Main {
     public static void main(String[] args) throws IOException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
         System.out.println(readAsString(new FileInputStream("task03/src/com/example/task03/input.test"), Charset.forName("KOI8-R")));
-        */
     }
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        // your implementation here
-        return "";
+        if (inputStream == null || !Charset.isSupported(charset.toString())) {
+            throw new IllegalArgumentException();
+        }
+        return new BufferedReader(new InputStreamReader(inputStream, charset)).readLine();
     }
 }
